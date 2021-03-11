d23acc7a19d950fec8dbddf13cca2fa650d5d7b0
marouxet <marianoarouxet@gmail.com>
2021-03-11 09:04:31 -0300

    Shopping based on amazon

---
+++ shopping.py
@@ -0,0 +1,149 @@
+import csv
+import sys
+import pandas as pd
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+TEST_SIZE = 0.4
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python shopping.py data")
+
+    # Load data from spreadsheet and split into train and test sets
+    
+    evidence, labels = load_data(sys.argv[1])
+    
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+
+    # Train model and make predictions
+    model = train_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    sensitivity, specificity = evaluate(y_test, predictions)
+
+    # Print results
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
+    print(f"True Negative Rate: {100 * specificity:.2f}%")
+
+
+def load_data(filename):
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of
+    evidence lists and a list of labels. Return a tuple (evidence, labels).
+
+    evidence should be a list of lists, where each list contains the
+    following values, in order:
+        - Administrative, an integer
+        - Administrative_Duration, a floating point number
+        - Informational, an integer
+        - Informational_Duration, a floating point number
+        - ProductRelated, an integer
+        - ProductRelated_Duration, a floating point number
+        - BounceRates, a floating point number
+        - ExitRates, a floating point number
+        - PageValues, a floating point number
+        - SpecialDay, a floating point number
+        - Month, an index from 0 (January) to 11 (December)
+        - OperatingSystems, an integer
+        - Browser, an integer
+        - Region, an integer
+        - TrafficType, an integer
+        - VisitorType, an integer 0 (not returning) or 1 (returning)
+        - Weekend, an integer 0 (if false) or 1 (if true)
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if Revenue is true, and 0 otherwise.
+    """
+    dataFrame = pd.read_csv(filename)
+
+
+    monthDic = {'Jan':0,'Feb':1,'Mar':2,'Apr':3,'May':4,'June':5,'Jul':6,'Aug':7,'Sep':8,'Oct':9,'Nov':10,'Dec':11}
+    dataFrame["Month"] = dataFrame["Month"].apply(lambda x:monthDic[x])
+
+    visitorTypeDic = {'Returning_Visitor':1, "New_Visitor": 0, "Other": 0}
+    dataFrame["VisitorType"] = dataFrame["VisitorType"].apply(lambda x:visitorTypeDic[x])
+
+    dataFrame["Weekend"] = dataFrame["Weekend"].apply(int)
+    dataFrame["Labels"] = dataFrame["Revenue"].apply(int)
+    
+    dataFrame.drop(["Revenue"], axis = 1, inplace = True)
+   
+    # Create an empty list 
+    evidence =[] 
+    labels =[] 
+
+    # Iterate over each row 
+    for index, rows in dataFrame.iterrows(): 
+        # Create list for the current row 
+        my_list =[int(rows.Administrative), rows.Administrative_Duration, int(rows.Informational),
+        rows.Informational_Duration, int(rows.ProductRelated) ,rows.ProductRelated_Duration,
+       rows.BounceRates, rows.ExitRates,rows.PageValues,rows.SpecialDay, int(rows.Month),
+       int(rows.OperatingSystems), int(rows.Browser), int(rows.Region), int(rows.TrafficType), int(rows.VisitorType),
+       int(rows.Weekend)]
+
+        my_other_list = int(rows.Labels)
+
+       # append the list to the final list 
+        evidence.append(my_list) 
+        labels.append(my_other_list)
+        
+    return evidence, labels
+
+def train_model(evidence, labels):
+    """
+    Given a list of evidence lists and a list of labels, return a
+    fitted k-nearest neighbor model (k=1) trained on the data.
+    """
+
+    knd =  KNeighborsClassifier(1)
+    knd.fit(evidence, labels)
+    return knd
+
+
+def evaluate(labels, predictions):
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return a tuple (sensitivity, specificty).
+
+    Assume each label is either a 1 (positive) or 0 (negative).
+
+    `sensitivity` should be a floating-point value from 0 to 1
+    representing the "true positive rate": the proportion of
+    actual positive labels that were accurately identified.
+
+    `specificity` should be a floating-point value from 0 to 1
+    representing the "true negative rate": the proportion of
+    actual negative labels that were accurately identified.
+    """
+    specificity = 0
+    sensitivity = 0
+     
+    total_negatives = 0
+    total_positives = 0
+    for x in range(len(labels)):
+        if labels[x] == 0:
+            total_negatives += 1
+            if predictions[x] == 0:
+                specificity += 1
+        elif labels[x] == 1:
+            total_positives += 1
+            if predictions[x] == 1:
+                sensitivity += 1        
+
+    specificity = specificity / total_negatives
+    sensitivity = sensitivity / total_positives
+
+    return sensitivity, specificity
+
+
+if __name__ == "__main__":
+    main()
