59029dc0df15709e6341ed122bc0a034b6da8ddd
marouxet <marianoarouxet@gmail.com>
2021-03-16 10:25:50 -0300

    baseok

--- questions.py
+++ questions.py
@@ -1,5 +1,9 @@
 import nltk
 import sys
+import os 
+import string
+import math
+#nltk.download('all')
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -12,12 +16,14 @@ def main():
         sys.exit("Usage: python questions.py corpus")
 
     # Calculate IDF values across files
+    #files = load_files("corpus")
     files = load_files(sys.argv[1])
     file_words = {
         filename: tokenize(files[filename])
         for filename in files
     }
     file_idfs = compute_idfs(file_words)
+
 
     # Prompt user for query
     query = set(tokenize(input("Query: ")))
@@ -48,7 +54,17 @@ def load_files(directory):
     Given a directory name, return a dictionary mapping the filename of each
     `.txt` file inside that directory to the file's contents as a string.
     """
-    raise NotImplementedError
+
+    dictLabels= {}
+  
+    for file  in os.listdir(directory):
+        textFile = open(os.path.join(directory,file),'r')
+        text = textFile.read() 
+        dictLabels.update({file:text})
+        
+    return dictLabels
+
+    
 
 
 def tokenize(document):
@@ -59,7 +75,15 @@ def tokenize(document):
     Process document by coverting all words to lowercase, and removing any
     punctuation or English stopwords.
     """
-    raise NotImplementedError
+    
+    tokens = nltk.tokenize.word_tokenize(document)
+    filterTokens = []
+    for word in tokens:
+        if (word not in nltk.corpus.stopwords.words("english")) and (word not in list(string.punctuation)):
+            filterTokens.append(word.lower())
+    filterTokens.sort()
+    return filterTokens
+    
 
 
 def compute_idfs(documents):
@@ -70,8 +94,20 @@ def compute_idfs(documents):
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
     """
-    raise NotImplementedError
+    idfs = dict()
+    for doc, str in documents.items():
+        words = set(str)
+        for word in words:
+            if word in idfs.keys():
+                idfs.update({word:idfs[word]+1})
+            else:
+                idfs.update({word:1})
 
+    for word, cantidad in idfs.items():
+        idfs.update({word:math.log(len(documents.keys())/cantidad)})        
+    return idfs
+    
+    
 
 def top_files(query, files, idfs, n):
     """
@@ -80,7 +116,22 @@ def top_files(query, files, idfs, n):
     to their IDF values), return a list of the filenames of the the `n` top
     files that match the query, ranked according to tf-idf.
     """
-    raise NotImplementedError
+    
+    tf_idf = dict.fromkeys(files.keys(),0)
+    words_checked = []
+   
+    for file, strs in files.items():
+        for word in query:
+            if (word in strs) and (word not in words_checked):
+                tf_idf.update({file:tf_idf[file]+strs.count(word) *  idfs[word]})
+                words_checked.append(word)
+            words_checked = []
+                
+    
+    orden = [x for x,y in sorted(tf_idf.items(), key=lambda item: item[1], reverse=True)]
+
+    return orden[:n]                  
+    
 
 
 def top_sentences(query, sentences, idfs, n):
@@ -91,7 +142,26 @@ def top_sentences(query, sentences, idfs, n):
     the query, ranked according to idf. If there are ties, preference should
     be given to sentences that have a higher query term density.
     """
-    raise NotImplementedError
+    idf = dict.fromkeys(sentences.keys(),0)
+    qtd = dict.fromkeys(sentences.keys(),0)
+    words_checked = []
+   
+    for sentence, strs in sentences.items():
+        wis = 0
+        for word in query:
+            if (word in strs) and (word not in words_checked):
+                idf.update({sentence:idf[sentence]+idfs[word]})
+                wis += 1
+                #qtd.update({sentence:strs.count(word)})
+                words_checked.append(word)
+            words_checked = []
+        qtd.update({sentence:wis/len(strs)})
+    
+    orden = sorted(idf.keys(), key = lambda x: (idf[x],qtd[x]), reverse = True)
+
+    return orden[:n]     
+    
+  
 
 
 if __name__ == "__main__":
